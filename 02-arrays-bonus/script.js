350a7ec701a3f48f92643c7b8a815560c90c25a8
AgostaSalvatore <agosta.boolean@gmail.com>
2025-03-19 14:44:32 +0100

    es 3 folder bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -26,9 +26,17 @@ for(let i = 0; i<teachers.length; i++){
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
+teachers.splice(1,1)
+console.log(teachers)
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+for(let i = 0; i<teachers.length; i++){
+  if(teachers[i] == 'Fabio'){
+    
+  }
+}
+// const isFabioPresent = null;
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
